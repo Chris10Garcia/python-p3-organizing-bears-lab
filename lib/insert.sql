b7ffb887446ea36bb6ada2a07de8196226faa6b7
Christian Garcia <bones192nd@gmail.com>
2023-06-21 20:37:41 -0400

    fun lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 14, "M", "brown", "cuddly", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Rowdy", 15, "M", "black", "angry", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Tabitha", 7, "F", "white", "caring", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Sergeant Brown", 9, "M", "grey", "honorable", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Melissa", 7, "F", "brown", "shy", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Grinch", 20, "M", "black", "mean", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Wendy", 10, "F", "grey", "angry", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 5, "F", "brown", "cuddly", TRUE);
